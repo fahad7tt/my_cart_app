f77ad2ad5db03a9eb8772f6401cb1a5d907c324a
Fahad TT <ttfahad2317@gmail.com>
2025-12-16 00:01:54 +0530

    bugs fixed

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'view/screens/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cart App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Poppins',
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
